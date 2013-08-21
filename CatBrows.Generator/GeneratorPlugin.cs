c71f1b1d3bd11ada7dba476875e52ed95f22eb4e
Sebastian Hallen <sebastian.hallen@gmail.com>
2013-08-21 16:27:52 +0200

    renamed project

---
+++ CatBrows.Generator/GeneratorPlugin.cs
@@ -0,0 +1,31 @@
+﻿using CatBrows.Generator;
+using TechTalk.SpecFlow.Infrastructure;
+
+[assembly: GeneratorPlugin(typeof(GeneratorPlugin))]
+namespace CatBrows.Generator
+{
+	using BrowserTestGenerator;
+	using BoDi;
+	using TechTalk.SpecFlow.Generator.Configuration;
+	using TechTalk.SpecFlow.Generator.Plugins;
+	using TechTalk.SpecFlow.Generator.UnitTestProvider;
+
+	public class GeneratorPlugin
+        : IGeneratorPlugin
+    {
+        public void RegisterDependencies(ObjectContainer container)
+        {
+            
+        }
+
+        public void RegisterCustomizations(ObjectContainer container, SpecFlowProjectConfiguration generatorConfiguration)
+        {
+            container.RegisterTypeAs<NUnitBrowserTestGenerator, IUnitTestGeneratorProvider>();
+        }
+
+        public void RegisterConfigurationDefaults(SpecFlowProjectConfiguration specFlowConfiguration)
+        {
+            
+        }
+    }
+}
