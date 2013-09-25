b2da439a69c8d6b3e6cb1f42a3c6156069a18345
Sebastian Hallén <sebastian.hallen@gmail.com>
2013-09-25 19:15:09 +0200

    run tests in build script

--- build.ps1
+++ build.ps1
@@ -10,5 +10,5 @@ $msbuildExe = join-path -path (Get-ItemProperty $regKey).$regProperty -childpath
 Copy-Item -Force -Path CatBrows.Generator\bin\Debug\*.* -Destination Tests\plugin
 #Remove-Item -Path Tests\*.feature.cs
 #echo "" > .\Tests\BrowserTest.feature.cs
-packages\SpecFlow.1.9.0\tools\specflow.exe generateall Tests\CatBrows.Generator.Tests.csproj
-&$msbuildExe Tests\CatBrows.Generator.Tests.csproj
+&packages\SpecFlow.1.9.0\tools\specflow.exe generateall Tests\CatBrows.Generator.Tests.csproj
+&$msbuildExe Tests\CatBrows.Generator.Tests.csproj¨&.\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe Tests\bin\Debug\CatBrows.Generator.Tests.dll
