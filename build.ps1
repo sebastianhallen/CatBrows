403e39d36ae944dd7f044b0e9908ca0e74a1bcc8
Sebastian Hallén <sebastian.hallen@gmail.com>
2014-01-04 23:29:49 +0100

    move source code into src folder

--- build.ps1
+++ build.ps1
@@ -3,23 +3,23 @@ $regKey = "HKLM:\software\Microsoft\MSBuild\ToolsVersions\$dotNetVersion"
 $regProperty = "MSBuildToolsPath"
 
 $msbuildExe = join-path -path (Get-ItemProperty $regKey).$regProperty -childpath "msbuild.exe"
-$specflowFolder = ".\packages\SpecFlow.1.9.0\tools"
+$specflowFolder = ".\src\packages\SpecFlow.1.9.0\tools"
 $specflowExe = $specflowFolder + "\specflow.exe"
-$nunitExe = ".\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe"
+$nunitExe = ".\src\packages\NUnit.Runners.2.6.2\tools\nunit-console.exe"
 
 
 
-&$msbuildExe CatBrows.Generator\CatBrows.Generator.csproj
-Copy-Item -Force -Path CatBrows.Generator\bin\Debug\CatBrows.Generator.SpecFlowPlugin.dll -Destination Tests\plugin
+&$msbuildExe src\CatBrows.Generator\CatBrows.Generator.csproj
+Copy-Item -Force -Path src\CatBrows.Generator\bin\Debug\CatBrows.Generator.SpecFlowPlugin.dll -Destination src\Tests\plugin
 
 Write-Host "###################################"
 Write-Host "regenerating specflow files"
-Copy-Item -Force -Path .\packages\specflow.exe.config -Destination $specflowFolder
-&$specflowExe generateall TestSample.BrowserGuardDisabled\TestSample.BrowserGuardDisabled.csproj /force /verbose&$specflowExe generateall TestSample.BrowserGuardEnabled\TestSample.BrowserGuardEnabled.csproj /force /verbose&$specflowExe generateall TestSample.DefaultSettings\TestSample.DefaultSettings.csproj /force /verbose
+Copy-Item -Force -Path .\src\packages\specflow.exe.config -Destination $specflowFolder
+&$specflowExe generateall src\TestSample.BrowserGuardDisabled\TestSample.BrowserGuardDisabled.csproj /force /verbose&$specflowExe generateall src\TestSample.BrowserGuardEnabled\TestSample.BrowserGuardEnabled.csproj /force /verbose&$specflowExe generateall src\TestSample.DefaultSettings\TestSample.DefaultSettings.csproj /force /verbose
 Write-Host ""
 
 
 Write-Host "###################################"
 Write-Host "rebuilding test projects with regenerated spec flow files"
-&$msbuildExe /t:Build Tests\CatBrows.Generator.Tests.csproj&$msbuildExe /t:Build TestSample.BrowserGuardDisabled\TestSample.BrowserGuardDisabled.csproj&$msbuildExe /t:Build TestSample.BrowserGuardEnabled\TestSample.BrowserGuardEnabled.csproj&$msbuildExe /t:Build TestSample.DefaultSettings\TestSample.DefaultSettings.csprojWrite-Host ""
-&$nunitExe Tests\bin\Debug\CatBrows.Generator.Tests.dll TestSample.BrowserGuardDisabled\bin\Debug\TestSample.BrowserGuardDisabled.dll TestSample.BrowserGuardEnabled\bin\Debug\TestSample.BrowserGuardEnabled.dll TestSample.DefaultSettings\bin\Debug\TestSample.DefaultSettings.dll
+&$msbuildExe /t:Build src\Tests\CatBrows.Generator.Tests.csproj&$msbuildExe /t:Build src\TestSample.BrowserGuardDisabled\TestSample.BrowserGuardDisabled.csproj&$msbuildExe /t:Build src\TestSample.BrowserGuardEnabled\TestSample.BrowserGuardEnabled.csproj&$msbuildExe /t:Build src\TestSample.DefaultSettings\TestSample.DefaultSettings.csprojWrite-Host ""
+&$nunitExe src\Tests\bin\Debug\CatBrows.Generator.Tests.dll src\TestSample.BrowserGuardDisabled\bin\Debug\TestSample.BrowserGuardDisabled.dll src\TestSample.BrowserGuardEnabled\bin\Debug\TestSample.BrowserGuardEnabled.dll src\TestSample.DefaultSettings\bin\Debug\TestSample.DefaultSettings.dll
